28429d48e8315d89a8dbade8607ced2ee57df8d3
cdi-lori <lorie@codedisruptors.com>
2018-12-03 18:20:52 +0800

    withdraw revised

--- index.js
+++ index.js
@@ -16,13 +16,12 @@ module.exports = function(db_name) {
     var item_config = config[db_name + "_item_config"];
     var user_config = config[db_name + "_user_config"];
 
-    module.deposit = (data) => {
+    module.withdraw = (data) => {
         return new Promise(function(resolve, reject) {
     
             const datum = data[0];
     
-            function check_location(cb){
-    
+            function check_location(cb){    
                 for(let i=0; i<datum.items.length; i++){
                     mysql.use(db)
                         .query(
@@ -37,7 +36,7 @@ module.exports = function(db_name) {
                                     }else{
                                         datum.items[i].id = uuid.v4();
                                         datum.items[i].user_id = datum.user_id;
-                                        datum.items[i].type = "DEPOSIT"
+                                        datum.items[i].type = "WITHDRAW"
     
                                         if(datum.items[i].expiration_date == undefined){
                                             datum.items[i].expiration_date = null
@@ -57,15 +56,77 @@ module.exports = function(db_name) {
                 }
     
             }
+
+
+            function check_quantity(cb){
+
+                for(let i=0; i<datum.items.length; i++){
+
+                    if(datum.items[i].quantity == 0){
+                        reject("There is no withdraw quantity, no items withdrawn");
+                    }
+
+                    mysql.use(db)
+                        .query(
+                            'SELECT quantity, type FROM im_item_movement WHERE item_id = ? AND location_id = ? AND user_id =? AND deleted IS NULL',
+                            [datum.items[i].item_id, datum.items[i].location_id, datum.items[i].user_id],
+                            function(error, result) {
+                                if(error) {
+                                    reject(error);
+                                }else{
+                                    if(result.length==0){
+                                        return cb(null,false);
+                                    }
+                                                                                                          
+                                    let deposit = 0;
+                                    let withdraw = 0;
+
+                                    for(let a=0; a<result.length; a++){
+                                        if(result[a].type === "DEPOSIT"){
+                                            deposit +=result[a].quantity
+                                        }else if(result[a].type === "WITHDRAW"){
+                                            withdraw +=result[a].quantity
+                                        }
+
+                                        if(a==result.length-1){
+                                            let remaining = deposit - withdraw;
+
+                                            if(remaining == 0){
+                                                return reject("There is no quantity to withdraw");
+                                            }
+
+                                            if(remaining < datum.items[i].quantity){
+                                                return reject("Withdraw quantity exceeded, no items where withdrawn");
+                                            }
+                                            
+                                            if(i == datum.items.length-1){
+                                                return cb(null, true)
+                                            }
+
+                                        }
+                                    }
+                                    
+                                }
+                            }
+                        ).end();                        
+                        
+
+                }
     
-            async.series([check_location], (err, results) => {
+            }
+
+    
+            async.series([check_location, check_quantity], (err, results) => {
                 if (err) {
-                    return next(err);
+                    reject(err)
                 }
     
                 if(results[0]==false){
                     reject("Location not found no items were saved");
-                }else{
+                }
+                
+                if(results[0]==true && results[1]==true){
+                   
                     const transaction_id = uuid.v4();
 
                     mysql.use(db)
@@ -87,7 +148,7 @@ module.exports = function(db_name) {
                                                 }else {
                                                     if(i == datum.items.length-1){
                 
-                                                        resolve([datum.items, {message: "Items successfully deposited", transaction_id: transaction_id}]);
+                                                        resolve([datum.items, {message: "Items successfully withdrawn", transaction_id: transaction_id}]);
                                                     }
                                                 }
                                             }
@@ -97,8 +158,6 @@ module.exports = function(db_name) {
                                 }
                             }
                         ).end()
-
-    
                 }
     
             })
